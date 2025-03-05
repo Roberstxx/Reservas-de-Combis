714e579d733b192b0ff308f8ce077a9a3dbe28c2
roberstxx <robertomartin942@gmail.com>
2025-03-05 17:03:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import tkinter as tk
-from tkinter import messagebox
-from tkinter import Toplevel
+from tkinter import messagebox, Toplevel
 from Agregar_combi import CombiApp  # Importar la clase
-
+from Catalogo_combis import abrir_catalogo  # Importar la función correcta
 
 def abrir_agregar_combi():
-    ventana = Toplevel()  # Crear una nueva ventana
+    ventana = Toplevel(root)  # Crear una nueva ventana
     app = CombiApp(ventana)  # Iniciar la interfaz en la nueva ventana
-    ventana.mainloop()
 
 def editar_combi():
     ventana = tk.Toplevel(root)
@@ -26,12 +24,6 @@ def registrar_viaje():
     ventana.title("Registrar Viaje")
     ventana.geometry("400x300")
     tk.Label(ventana, text="Formulario para Registrar Viaje").pack(pady=20)
-
-def catalogo():
-    ventana = tk.Toplevel(root)
-    ventana.title("Catálogo")
-    ventana.geometry("400x300")
-    tk.Label(ventana, text="Sección de Catálogo").pack(pady=20)
 
 def administrar_usuario():
     ventana = tk.Toplevel(root)
@@ -53,7 +45,6 @@ frame.pack(pady=10)
 btn_agregar_combi = tk.Button(frame, text="Agregar combi", command=abrir_agregar_combi)
 btn_agregar_combi.grid(row=0, column=0, padx=5, pady=5)
 
-
 btn_editar = tk.Button(frame, text="Editar combi", command=editar_combi)
 btn_editar.grid(row=1, column=0, padx=5, pady=5)
 
@@ -63,7 +54,7 @@ btn_costo_ruta.grid(row=0, column=1, padx=5, pady=5)
 btn_registrar_viaje = tk.Button(frame, text="Registrar viaje", command=registrar_viaje)
 btn_registrar_viaje.grid(row=0, column=2, padx=5, pady=5)
 
-btn_catalogo = tk.Button(frame, text="Catálogo", command=catalogo)
+btn_catalogo = tk.Button(frame, text="Catálogo", command=abrir_catalogo)  # Solo abre cuando se presiona
 btn_catalogo.grid(row=0, column=3, padx=5, pady=5)
 
 btn_admin_usuario = tk.Button(frame, text="Administrar usuario", command=administrar_usuario)
